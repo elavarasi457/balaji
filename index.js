020cf8e3726533b65e97129b7ebbb768dd67a1a6
ela <elavarasim457@gmail.com>
2025-06-22 18:09:37 +0530

    ela

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const e=require("express");
 const n=e();
-
+const hbs=require("hbs");
+n.set("view engine", "hbs");
+n.get('/',(req,res)=>{
+    res.render("Home.hbs");
+});
 n.listen(1234,()=>{
     console.log("port connected.");
 });
